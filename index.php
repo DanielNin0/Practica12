1c86e5c7496e9f4c541db4220a33883323b12473
DanielNin0 <30698961+DanielNin0@users.noreply.github.com>
2017-10-11 18:13:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <html>
+
     <head>
+
         <title>Practica 12-variables PHP</title>
+
         <meta charset="utf-8">
+
     </head>
 
+
+
     <body>
+
         <?php
+
         $valor1=5;
+
         $valor2= 20;
+
         echo "<h1>OPERACIONES ARITMETICAS</H1></br>";
+
         echo "Valor 1: ".$valor1.'<br/>';
+
         echo "Valor 2: ".$valor2.'<br/>';
-        $suma = $valo1 + $valor2;
+
+        $suma = $valor1 + $valor2;
+
         $resta = $valor1-$valor2;
+
         $multiplicacion = $valor1*$valor2;
+
         $divicion = $valor1/$valor2;
+
         echo "<br/>la suma es: ".$suma;
+
         echo "<br/>la resta es: ".$resta;
+
         echo "<br/>la multiplicacion es: ".$multiplicacion;
+
         echo "<br/>la divicion es: ".$divicion;
+
         ?>
-        
-    </body>
-</html>
